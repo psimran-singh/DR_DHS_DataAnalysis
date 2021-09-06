a1b7ded1682031b0b1ecf18da11a10ec8c3a0a5d
Prabhsimran Singh <psimran.singh@gmail.com>
2021-09-06 17:27:06 -0400

    Added code to select columns to use and added notes

--- Data Management.R
+++ Data Management.R
@@ -3,19 +3,32 @@ setwd("~/Rutgers Fall 2021/Multivariate Methods/DR 2013 DHS Data/DR_2013_DHS Hou
 library(foreign)
 data <- read.dta("DRPR61FL.DTA") #Household Recode
 
-data <- subset(data,select=c("hv024", #Region of Residence
+selectcolumns <- subset(data,select=c("hv024", #Region of Residence
                              "hv025", #Urban or Rural
                              "hv101", #Relationship to Head of Household
                              "hv104", #Sex of HH Member
                              "hv105", #Age of HH Member
+                             "hv108", #Education in year for HH Member
                              "hv115", #Marital Status of HH Member
+                             "hv129", #School Attendance status category
+                             "hv140", #Member has birth certificate
+                             "hv206", #HH has electricity
+                             "hv207", #HH has radio
+                             "hv208", #HH has TV
+                             "hv209", #HH has fridge
+                             "hv210", #HH has bicycle
+                             "hv211", #HH has motorcycle
+                             "hv212", #HH has car
                              "hv219", #Sex of HH Head
                              "hv220", #Age of HH Head
+                             "hv221", #HH has telephone
                              "hv225", #Share bathroom with other HH's
                              "hv227", #Has mosquito net
                              "hv237", #Anything done to water to make potable
                              "hv242", #HH has seperate room used as kitchen
-                             "sh25", #
-                             
-                             "hv108"))
+                             "hv244", #HH has land usable for agriculture
+                             "hv245",  #Hectares of land usable for agriculture
+                             "sh25" #Gotta figure this one out still
+                             ))
+
 
