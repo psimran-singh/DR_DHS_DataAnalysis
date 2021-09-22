3ba52f8597514b83339d9046d14d58a8e0696c42
Prabhsimran Singh <psimran.singh@gmail.com>
2021-09-22 18:39:04 -0400

    Added code to make charts for age and years of education

--- Exploratory Analysis.R
+++ Exploratory Analysis.R
@@ -37,6 +37,35 @@ detach(data2a)
 
 #For now I'll keep the dataset representative of all regions
 #Let's look at the subset of the data we will sample from instead
-summary(data3a)
-summarydata3a <- data3a[,c(2:36)]
-summarydata3a %>% tbl_summary
+summary(data4)
+summarydata4 <- data4[,c(2:30)]
+summarydata4 %>% tbl_summary
+
+summarysample1 <- sample1[,c(2:30)]
+summarysample1 %>% tbl_summary
+
+ageplot <- ggplot(data4, aes(y=age)) + 
+  geom_boxplot(alpha=0.3) +
+  theme(legend.position="none")
+ageplot
+
+educationplot <- ggplot(data4, aes(y=years_of_education)) + 
+  geom_boxplot(alpha=0.3) +
+  theme(legend.position="none")
+educationplot
+
+agechart <- ggplot(data4, aes(x=age))+
+  geom_bar(stat="count")+
+  scale_x_continuous(breaks = data4$age) +
+  theme_minimal() +
+  ggtitle("Distribution of Age")+
+  theme(plot.title = element_text(hjust = 0.5))
+agechart
+
+educationchart <- ggplot(data4, aes(x=years_of_education))+
+  geom_bar(stat="count")+
+  scale_x_continuous(breaks = data4$years_of_education) +
+  theme_minimal() +
+  ggtitle("Distribution of Years of Education")+
+  theme(plot.title = element_text(hjust = 0.5))
+educationchart
